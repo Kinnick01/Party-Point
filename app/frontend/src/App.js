a30359306f76b8304749db2520e6c77c1bc08bd4
csakafeda <bako.feodora97@gmail.com>
2023-06-05 12:28:18 +0200

    add really basic home page

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import './App.css';
+import HomePage from './Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     This is the page.
+      <Router>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
