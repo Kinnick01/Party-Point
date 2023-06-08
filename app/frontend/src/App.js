8a5833bf499ebd2f00d74cf97776df79f3fd265c
csakafeda <bako.feodora97@gmail.com>
2023-06-08 21:51:14 +0200

    add navbar and logobar to the App

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import './App.css';
-import HomePage from './Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import HomePage from "./components/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import LogoBar from "./components/LogoBar/LogoBar";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Navbar />
+                <div className="content">
+                    <LogoBar/>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
