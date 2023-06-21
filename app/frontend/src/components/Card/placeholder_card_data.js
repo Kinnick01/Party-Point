0818ede011982d11d9779853b900d540dc08379e
csakafeda <bako.feodora97@gmail.com>
2023-06-21 19:44:25 +0200

    add placeholder data to the cards

---
+++ app/frontend/src/components/Card/placeholder_card_data.js
@@ -0,0 +1,25 @@
+export const placeholder_card_data = {
+    events: [
+        {
+            name: "EventName",
+            time: "20:00 - 06:00",
+            date: "01.01.2020 - 02.01.2020",
+            location: "Budapest",
+            picture: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/36/37/32/caption.jpg?w=700&h=500&s=1"
+        },
+        {
+            name: "EventName2",
+            time: "20:00 - 06:00",
+            date: "01.01.2020 - 02.01.2020",
+            location: "Budapest",
+            picture: ""
+        },
+        {
+            name: "EventName2",
+            time: "20:00 - 06:00",
+            date: "01.01.2020 - 02.01.2020",
+            location: "Budapest",
+            picture: ""
+        }
+    ]
+}
